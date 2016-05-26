0ed3ce8fec825b057b6234981e7571dd7c364459
Kim <kim.hajduch@saggezza.com>
2016-05-26 15:27:29 -0500

    Updating links - WIP; added about page

--- js/function.js
+++ js/function.js
@@ -8,25 +8,37 @@
 // 	// }
 // }
 
-$(function() {
-	// findViewportWidth();
+$(document).ready(function() {
+	checkUrl();
+  $('#portfolio').find('.section.what[id!=section-filter-web]').fadeOut();
 
 	$(document).on('click', '.navbar li.filter', function() {
 		$('.navbar li.active').removeClass('active');
 		$(this).addClass('active');
-
-		var filter = $(this).find('a').attr('id');
-		// console.log(filter);
-
-		if (filter === 'filter-all') {
-			$('#portfolio').find('img[class^=filter-]').fadeIn();
-		} else {
-			$('#portfolio').find('img[class!=' + filter + ']').fadeOut();
-			$('#portfolio').find('img[class=' + filter + ']').fadeIn();
-		}
+    checkUrl();
 	});
 
 	// window.onresize = function(event) {
 	// 	findViewportWidth();
 	// };
 });
+
+function checkUrl() {
+  console.log('hash', window.location.hash);
+
+  var filter = window.location.hash.replace('#', '');
+  console.log('filter', filter);
+
+  if (window.location.hash === '') {
+    changeSection('filter-web');
+  } else {
+    changeSection(filter);
+  }
+}
+
+function changeSection(filter) {
+  $('#portfolio').find('.section.what[id!=section-' + filter + ']').fadeOut(300, function() {
+    console.log('complete');
+    $('#portfolio').find('.section.what[id=section-' + filter + ']').delay(300).fadeIn('slow');
+  });
+}
