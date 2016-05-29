16816cab06a09f0b83fa8c87363235a8d18fc0c2
strangeapparition <strangeapparition@users.noreply.github.com>
2016-05-28 19:48:36 -0500

    Updating links

--- js/function.js
+++ js/function.js
@@ -1,44 +1,15 @@
-// function findViewportWidth() {
-// 	var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-
-// 	// if (w <= 767) {
-// 	// 	$('.pull-right').removeClass('nav-stacked');
-// 	// } else {
-// 	// 	$('.nav-pills').addClass('nav-stacked');
-// 	// }
-// }
-
-$(document).ready(function() {
-	checkUrl();
-  $('#portfolio').find('.section.what[id!=section-filter-web]').fadeOut();
-
-	$(document).on('click', '.navbar li.filter', function() {
-		$('.navbar li.active').removeClass('active');
-		$(this).addClass('active');
-    checkUrl();
-	});
-
-	// window.onresize = function(event) {
-	// 	findViewportWidth();
-	// };
-});
-
-function checkUrl() {
-  console.log('hash', window.location.hash);
-
-  var filter = window.location.hash.replace('#', '');
-  console.log('filter', filter);
-
-  if (window.location.hash === '') {
-    changeSection('filter-web');
-  } else {
-    changeSection(filter);
-  }
-}
-
 function changeSection(filter) {
   $('#portfolio').find('.section.what[id!=section-' + filter + ']').fadeOut(300, function() {
     console.log('complete');
     $('#portfolio').find('.section.what[id=section-' + filter + ']').delay(300).fadeIn('slow');
   });
 }
+
+$(document).ready(function() {
+  $(document).on('click', '.navbar li.filter', function() {
+    $('.navbar li.active').removeClass('active');
+    $(this).addClass('active');
+    var filter = $(this).find('a').attr('id');
+    changeSection(filter);
+  });
+});
