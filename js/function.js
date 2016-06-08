1492ab7b79ed3662c81fa18faed9f30b65f0f7f9
strangeapparition <strangeapparition@users.noreply.github.com>
2016-06-07 23:36:13 -0500

    Disabling Lightbox on mobile (xs)

--- js/function.js
+++ js/function.js
@@ -5,11 +5,28 @@ function changeSection(filter) {
   });
 }
 
+var lightboxOnResize = function() {
+  if ($(window).width() < 767) {
+    $('a.lightbox-link').removeAttr('data-lightbox');
+  } else {
+    $('a.lightbox-link').attr('data-lightbox', 'lightbox');
+  }
+};
+
+// $(document).ready(lightboxOnResize);
+
 $(document).ready(function() {
+  lightboxOnResize();
+
   $(document).on('click', '.navbar li.filter', function() {
     $('.navbar li.active').removeClass('active');
     $(this).addClass('active');
     var filter = $(this).find('a').attr('id');
     // changeSection(filter);
   });
+});
+
+$(window).resize(function() {
+  console.log('resize');
+  lightboxOnResize();
 });
